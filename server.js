d8b7d8419dda4b5f190728b05e66c98abf3cede9
unknown <you@example.com>
2024-01-10 02:05:25 -0800

    backend_server_impl

--- server.js
+++ server.js
@@ -6,25 +6,49 @@ app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post("/register", (request, response) => {
-    console.log("Request body:", request.body);
+const registerUsersData = []
+app.post("/register", async (request, response) => {
     const { name, email, password, mobile } = request.body;
     try {
         if (name && email && password && mobile) {
+            if(registerUsersData.filter((user) => user.email.includes(email)).length === 0){
+                registerUsersData.push(request.body)
+            } else {
+                throw new Error("User already exist, Please login")
+            }
             return response.status(200).json(request.body);
         }
-        throw new Error("Please send valid info");
+        throw new Error("Please fill all fields");
     } catch(error) {
-        console.error('18== Error:', error.message);
-        return response.status(400).json({ error: error.message });
+        console.error('21==', error.message);
+        return response.status(400).json({error: error.message});
     }
 });
 
-app.get("/getApi", (req, res) => {
-    console.log("24== Request:", req);
-    return res.json("Hello");
+app.post("/login", async (request, response) => {
+    const {email, password} = request.body;
+    try {
+        if (email && password) {
+            if(registerUsersData.filter((user) => user.email.includes(email)).length){
+                return response.status(200).json(registerUsersData.find((val) => val.email === email));
+            } else {
+                throw new Error("New user, Create an Account")
+            }
+        }
+        throw new Error("Please fill all fields");
+    } catch(error) {
+        console.error('40==', error.message);
+        return response.status(400).json({error: error.message});
+    }
 });
 
+app.get("/users", async (req,res) => {
+    try {
+        return res.status(200).json(registerUsersData)
+    } catch(error) {
+        return res.status(400).json({error: "Failed to fetch users data"})
+    }
+})
 app.listen(3214, () => {
-    console.log("29== Server is running on port 3214");
+    console.log("29== Server is running on port 3214", registerUsersData);
 });

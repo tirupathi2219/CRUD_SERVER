3a25a86b284074a340a80fb4433f64a96de0c99b
unknown <you@example.com>
2024-01-10 22:58:24 -0800

    edit&del apis

--- server.js
+++ server.js
@@ -7,16 +7,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const registerUsersData = []
+let userId = 0;
 app.post("/register", async (request, response) => {
     const { name, email, password, mobile } = request.body;
     try {
         if (name && email && password && mobile) {
+            userId++;
             if(registerUsersData.filter((user) => user.email.includes(email)).length === 0){
-                registerUsersData.push(request.body)
+                registerUsersData.push({...request.body, id: userId})
             } else {
                 throw new Error("User already exist, Please login")
             }
-            return response.status(200).json(request.body);
+            console.log('19== register:', registerUsersData);
+            return response.status(200).json(registerUsersData);
         }
         throw new Error("Please fill all fields");
     } catch(error) {
@@ -49,6 +52,37 @@ app.get("/users", async (req,res) => {
         return res.status(400).json({error: "Failed to fetch users data"})
     }
 })
+
+app.put('/updateUser', async (req,resp) => {
+    const { name, email, password, mobile, id } = req.body;
+    try {
+        if(name && email && password && mobile) {
+            if(registerUsersData.find((item) => item.id === id)) {
+                registerUsersData[registerUsersData.findIndex(val => val.id === id)] = req.body
+                console.log('61==', registerUsersData)
+                return resp.status(200).json(registerUsersData)
+            }
+        } else {
+            throw new Error("Fill required fields")
+        }
+    } catch(error) {
+        return resp.status(400).json({error: error.message })
+    }
+})
+
+app.delete('/deleteUser', async (req, resp) => {
+    try {
+        if(registerUsersData.find((item) => item.id === req.body.id)) {
+            registerUsersData.splice(registerUsersData.findIndex(val => val.id === req.body.id), 1)
+            console.log('77==', registerUsersData);
+            return resp.status(200).json(registerUsersData)
+        }
+        throw new Error('something went wrong')
+    } catch(error) {
+        return resp.status(400).json({error: error.message})
+    }
+})
+
 app.listen(3214, () => {
     console.log("29== Server is running on port 3214", registerUsersData);
 });

590331ba0959de44d761b9d70073ebf7b4fa8e06
tirupathi2219 <tirupathi2219@gmail.com>
2024-07-02 14:35:33 +0530

    123

--- DBConnection.js
+++ DBConnection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URL = "mongodb+srv://ssavsm:ssavsm143@cluster0.3sjg1ti.mongodb.net/"
 
-mongoose.connect(MONGODB_URL).then(() => {
+mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log('DATABASE CONNECTED....')
 }).catch(err => {
     console.log("DB CONNECTION ISSUE", err)
